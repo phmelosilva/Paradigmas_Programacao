e85fedc12eca4a2175d51f14e253097f141bbad1
phmelosilva <pedrodsm1819@gmail.com>
2025-09-11 21:32:57 -0300

    questoes lista 2 - Logico

--- 1_Prova/1_programa_prolog.pl
+++ 1_Prova/1_programa_prolog.pl
@@ -3,9 +3,13 @@ unb(X) :- fcte(X).
 fcte(ana).
 fcte(beto).
 fcte(carlos).
+fcte(lucas).
+fcte(marcos).
+
+iesb(maria).
 
 unb_report :-
     write('Estudantes da UnB: '), nl,
     unb(X),
     write(X), nl,
-    fail.
+    fail. # força a inserção diretamente, até achar um true
---
+++ 1_Prova/unificacao.pl
@@ -0,0 +1,12 @@
+f(1, 2).
+f(1, 3).
+f(2, 3).
+
+g(1).
+g(2).
+
+# Exemplos de consultas -> bateu nome, aridade e argumentos: true
+# ?- f(1, 2).
+# true
+# :- f(2, 1). -> bateu nome, aridade mas errou nos argumentos: false
+# false
