52fc5b8b6e062e5a066b41443d97b1ab59bb636f
phmelosilva <pedrodsm1819@gmail.com>
2025-09-14 20:33:02 -0300

    finaliza questao 4 - Lista1 Logico

--- 1_Prova/questao_4_Lista1Logico.pl
+++ 1_Prova/questao_4_Lista1Logico.pl
@@ -26,3 +26,18 @@ matricula('APC', ian).
 
 % letra (a) - Implemente a regra tem_calouros/1, que retorna as disciplinas que tem calouros matriculados
 tem_calouros(X) :- matricula(X, Y), estudante(Y, 1, _).
+
+% letra (b) - Implemente a regra turma_mista/1, que retorna as disciplinas que tem ao menos um estudante de cada curso.
+turma_mista(X) :- 
+    matricula(X, Y), 
+    estudante(Y, _, 'Engenharia de Software'),
+    matricula(X, Z),
+    estudante(Z, _, 'Engenharias'),
+    matricula(X, W),
+    estudante(W, _, 'Engenharia de Energia').
+
+% letra (c) - Implemente a regra software/0, que imprime todos os estudantes de Engenharia de Software, um estudante por linha.
+software :- 
+    estudante(X, _, 'Engenharia de Software'), 
+    write(X), nl, 
+    fail.
