9aa863bf1bbd43b08275ffb2aae5000611fc7b7c
phmelosilva <pedrodsm1819@gmail.com>
2025-09-22 17:13:10 -0300

    soma com recursao de cauda

--- 1_Prova/recursao_exercicios.pl
+++ 1_Prova/recursao_exercicios.pl
@@ -9,3 +9,13 @@ soma_lista([H|T],  S) :-
 
 soma_lista([], 0).
 
+% Caso Base da Recursão com Cauda
+
+soma_lista_tail(Lista, S) :-
+    soma_lista_tr(Lista, 0, S).
+
+soma_lista_tr([], Acc, Acc).
+
+soma_lista_tr([H|T], Acc, Soma) :-
+    NovoAcc is Acc+H,
+    soma_lista_tr(T, NovoAcc, Soma).
