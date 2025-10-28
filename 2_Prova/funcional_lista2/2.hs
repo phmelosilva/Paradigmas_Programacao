3f0c78591348129295d80179d4ae071ef12c4234
phmelosilva <pedrodsm1819@gmail.com>
2025-10-27 21:08:39 -0300

    parte das questoes lista 2 funcional

---
+++ 2_Prova/funcional_lista2/2.hs
@@ -0,0 +1,14 @@
+-- Usando uma unica dobra a esquerda, implemente as funcoes abaixo. A implementacao deve ser feita em uma unica  ́linha.
+
+-- a) eval :: [Int] -> Int -> Int, que recebe uma lista de inteiros as e um inteiro x e retorna o valor de p(x), onde
+-- Exemplo: ghci> eval [1 -5 6] 2 -- 0
+-- [1 -5 6] 2 seria: x² -5x + 6 -> 4 - 10 + 6 = 0
+eval (x:xs) y = foldl (\acc c -> (acc * y) + c) 0 (x:xs)
+
+
+-- c) mean :: [Int] -> Double, que recebe uma lista de inteiros xs e retorna a media aritmetica destes numeros
+-- ghci> mean [1..4] -- 2.5
+-- 1 + 2 + 3 + 4 = 10, que 10/4 dá 2.5
+mean :: [Int] -> Double
+mean (x:xs) = (fromIntegral $ foldl (+) 0 (x:xs)) / (fromIntegral $ length (x:xs))
+
